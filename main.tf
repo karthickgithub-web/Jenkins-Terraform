c61ea66cda3d31d71f79bb93c439dad614f722a0
anil <polagonianilgoud@gmail.com>
2024-11-03 13:08:26 +0530

    instance name changed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "demo1" {
+resource "aws_instance" "demo2" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
