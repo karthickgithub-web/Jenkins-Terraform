60ea0b6e7add1bb6233ee05ae713973244dff80d
karthickgithub-web <110174311+karthickgithub-web@users.noreply.github.com>
2024-12-24 10:55:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "ec2-2024" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Terraform-Instance"
   }
 }
